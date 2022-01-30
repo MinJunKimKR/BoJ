63c011436ac6d989d092fd3fbcef2cb913ecb52d
MinjunKim <36980186+MinJunKimKR@users.noreply.github.com>
2022-01-31 02:58:29 +0900

    [Silver V] Title: 수 정렬하기 3, Time: 9304 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Silver/10989.수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,17 @@
+import sys
+sys_input = sys.stdin.readline
+
+N = int(input())
+nums = [0]*10001
+for _ in range(N):
+    nums[int(sys_input())] += 1
+
+num_str = ''
+for idx in range(1, len(nums)):
+    if N < 1:
+        break
+    if nums[idx] == 0:
+        continue
+    for _ in range(nums[idx]):
+        N -= 1
+        print(idx)
