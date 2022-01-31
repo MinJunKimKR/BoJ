4a1d58f7ee3ce9e29b98b7158ad08faf350b04ba
MinjunKim <36980186+MinJunKimKR@users.noreply.github.com>
2022-01-31 18:49:14 +0900

    [Silver IV] Title: 괄호, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/9012.괄호/괄호.py
@@ -0,0 +1,24 @@
+import sys
+sys_input = sys.stdin.readline
+
+N = int(input())
+
+for _ in range(N):
+    stack = []
+    baskets = list(sys_input())
+    for basket in baskets:
+        if basket == '\n':
+            continue
+
+        if basket != '(':
+            if len(stack) > 0 and stack[len(stack)-1] == '(':
+                stack.pop()
+            else:
+                stack.append(basket)
+        else:
+            stack.append(basket)
+
+    if len(stack) == 0:
+        print('YES')
+    else:
+        print('NO')
