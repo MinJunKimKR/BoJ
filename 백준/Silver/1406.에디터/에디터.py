45ace615498bd9a3fcbf4ce88f960242679b2ce9
MinjunKim <36980186+MinJunKimKR@users.noreply.github.com>
2022-02-02 23:40:02 +0900

    [Silver III] Title: 에디터, Time: 336 ms, Memory: 37180 KB -BaekjoonHub

---
+++ 백준/Silver/1406.에디터/에디터.py
@@ -0,0 +1,28 @@
+import sys
+sys_input = sys.stdin.readline
+
+left_stack = list(input())
+right_stack = []
+N = int(input())
+cursor = len(left_stack)
+
+
+for _ in range(N):
+    command = sys_input()
+    if command[0] == 'P':
+        left_stack.append(command[2])
+        continue
+    if command[0] == 'L' and len(left_stack) > 0:
+        right_stack.append(left_stack.pop())
+        continue
+    if command[0] == 'D' and len(right_stack) > 0:
+        left_stack.append(right_stack.pop())
+        continue
+    if command[0] == 'B' and len(left_stack) > 0:
+        left_stack.pop()
+        continue
+
+while len(right_stack) > 0:
+    left_stack.append(right_stack.pop())
+
+print(''.join(left_stack))
