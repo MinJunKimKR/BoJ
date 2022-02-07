78492fa0433e191ceb25e2234e8c97eb2513855d
MinjunKim <36980186+MinJunKimKR@users.noreply.github.com>
2022-02-07 22:27:15 +0900

    [Bronze I] Title: 진법 변환 2, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/11005.진법 변환 2/진법 변환 2.py
@@ -0,0 +1,10 @@
+n, b = map(int, input().split())
+
+zinsu = [str(x) for x in range(10)] + [chr(x)
+                                       for x in range(ord('A'), ord('Z')+1)]
+
+nums = []
+while n > 0:
+    nums.append(zinsu[(n % b)])
+    n = n // b
+print(''.join(nums[::-1]))
