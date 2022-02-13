788aebc5f721c2f3ce0f42de95415027de2bb673
MinjunKim <36980186+MinJunKimKR@users.noreply.github.com>
2022-02-13 23:04:19 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 4320 ms, Memory: 129500 KB -BaekjoonHub

---
+++ 백준/Silver/10816.숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,33 @@
+import sys
+sys_input = sys.stdin.readline
+dic = {}
+
+N = int(input())
+n_cards = list(map(int, sys_input().strip().split()))
+n_cards.sort()
+
+for n in n_cards:
+    if dic.get(n) == None:
+        dic[n] = 1
+    else:
+        dic[n] += 1
+M = int(input())
+m_cards = list(map(int, sys_input().strip().split()))
+
+
+def bi_search(start, end, num):
+    if start > end:
+        print(0, end=' ')
+        return
+    mid = (start + end)//2
+    if n_cards[mid] == num:
+        print(dic[num], end=' ')
+        return
+    if n_cards[mid] > num:
+        bi_search(start, mid-1, num)
+    else:
+        bi_search(mid+1, end, num)
+
+
+for m in m_cards:
+    bi_search(0, N-1, m)
