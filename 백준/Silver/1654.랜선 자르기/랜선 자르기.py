82f28c0dabe76276c4903a4718dc0248df13e0b2
MinjunKim <36980186+MinJunKimKR@users.noreply.github.com>
2022-02-13 17:54:53 +0900

    [Silver III] Title: 랜선 자르기, Time: 496 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/1654.랜선 자르기/랜선 자르기.py
@@ -0,0 +1,25 @@
+K, N = map(int, input().split())
+
+lan_lines = []
+for _ in range(K):
+    lan_lines.append(int(input()))
+max_size = 0
+
+
+def search_line(start, end):
+    if start > end:
+        return
+    global max_size
+    line_cnt = 0
+    mid = int((start+end)/2)
+    for line in lan_lines:
+        line_cnt += line//mid
+    if line_cnt >= N:
+        max_size = max(max_size, mid)
+        search_line(mid+1, end)
+    else:
+        search_line(start, mid-1)
+
+
+search_line(1, max(lan_lines))
+print(max_size)
