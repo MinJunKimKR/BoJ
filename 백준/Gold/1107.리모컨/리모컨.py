8951af38708fb6668d7493c4ba6ecea14df874a1
MinjunKim <36980186+MinJunKimKR@users.noreply.github.com>
2022-02-19 02:43:48 +0900

    [Gold V] Title: 리모컨, Time: 2408 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Gold/1107.리모컨/리모컨.py
@@ -0,0 +1,18 @@
+N = int(input())
+M = int(input())
+broken = []
+if M > 0:
+    broken = input().split()
+ch_num = 100
+cnt_click = abs(N-ch_num)
+
+
+for num in range(1000001):
+    str_num = str(num)
+    for i in range(len(str_num)):
+        if str_num[i] in broken:
+            break
+        elif i == len(str_num)-1:
+            cnt_click = min(cnt_click, abs(int(str_num) - N) + len(str_num))
+
+print(cnt_click)
