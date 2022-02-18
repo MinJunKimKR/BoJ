86960bab93be2387440545728f4227333f39c27e
MinjunKim <36980186+MinJunKimKR@users.noreply.github.com>
2022-02-18 21:17:16 +0900

    [Silver III] Title: 동전 0, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/11047.동전 0/동전 0.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+coins = []
+cnt_coin = 0
+for _ in range(N):
+    coins.append(int(input()))
+
+
+for i in range(N-1, -1, -1):
+    coin = coins[i]
+    if coin > K:
+        continue
+    cnt_coin += K//coin
+    K = K % coin
+
+print(cnt_coin)
