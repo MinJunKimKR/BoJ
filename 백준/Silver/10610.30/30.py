9bad1d4550f809f48632745c3e48d1fd172d9104
MinjunKim <36980186+MinJunKimKR@users.noreply.github.com>
2022-02-18 22:01:45 +0900

    [Silver V] Title: 30, Time: 148 ms, Memory: 32424 KB -BaekjoonHub

---
+++ 백준/Silver/10610.30/30.py
@@ -0,0 +1,11 @@
+N = list(input())
+
+if N.count('0') == 0 or int(''.join(N)) < 30:
+    print(-1)
+    exit(0)
+if sum(map(int, N)) % 3 != 0:
+    print(-1)
+    exit(0)
+
+nums = sorted(N, reverse=True)
+print(''.join(nums))
