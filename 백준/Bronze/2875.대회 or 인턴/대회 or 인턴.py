43fcb883492d2744ec8b541d520f1c4f3063fd60
MinjunKim <36980186+MinJunKimKR@users.noreply.github.com>
2022-02-18 21:26:27 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/2875.대회 or 인턴/대회 or 인턴.py
@@ -0,0 +1,14 @@
+N, M, K = map(int, input().split())
+
+
+for _ in range(K):
+    if N > M*2:
+        N -= 1
+        # continue
+    else:
+        M -= 1
+
+if N > M*2:
+    print(M)
+else:
+    print(N//2)
