3f7612a752294e911a52d96af47469288909029c
MinjunKim <36980186+MinJunKimKR@users.noreply.github.com>
2022-03-07 18:35:38 +0900

    [Silver III] Title: N과 M (3), Time: 844 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+
+nums = [x for x in range(1, N+1)]
+
+
+def dfs(arr_num):
+    global nums
+    if len(arr_num) == M:
+        print(' '.join(arr_num))
+        return
+    for num in nums:
+        arr_num.append(str(num))
+        dfs(arr_num)
+        arr_num.pop()
+
+
+dfs([])
