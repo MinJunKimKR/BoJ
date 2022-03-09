3b788c590210fffcdf4d4673ee8735d35feefc68
MinjunKim <36980186+MinJunKimKR@users.noreply.github.com>
2022-03-09 16:46:42 +0900

    [Silver I] Title: 맥주 마시면서 걸어가기, Time: 104 ms, Memory: 32420 KB -BaekjoonHub

---
+++ 백준/Silver/9205. 맥주 마시면서 걸어가기/README.md
@@ -0,0 +1,12 @@
+# [Silver I] 맥주 마시면서 걸어가기 - 9205 
+
+[문제 링크](https://www.acmicpc.net/problem/9205) 
+
+### 성능 요약
+
+메모리: 32420 KB, 시간: 104 ms
+
+### 분류
+
+너비 우선 탐색(bfs), 그래프 이론(graphs), 그래프 탐색(graph_traversal)
+
---
+++ 백준/Silver/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -0,0 +1,43 @@
+from collections import deque
+import sys
+sys_input = sys.stdin.readline
+T = int(input())
+
+
+def is_reach(x, y, nx, ny):
+    dist = abs(x-nx) + abs(y-ny)
+    return True if dist <= 1000 else False
+
+
+def bfs():
+    q = deque([])
+    q.append((sx, sy))
+    while q:
+        x, y = q.popleft()
+        if is_reach(x, y, gx, gy):
+            print("happy")
+            return True
+        for idx in range(N):
+            if visited[idx]:
+                continue
+            nx, ny = convi[idx]
+            if is_reach(x, y, nx, ny):
+                visited[idx] = True
+                q.append((nx, ny))
+    return False
+
+
+for _ in range(T):
+    N = int(input())
+    sx, sy = map(int, input().split())
+    convi = []
+    for _ in range(N):
+        x, y = map(int, sys_input().strip().split())
+        convi.append((x, y))
+    gx, gy = map(int, input().split())
+    if is_reach(sx, sy, gx, gy):
+        print('happy')
+        continue
+    visited = [False] * (N)
+    if not bfs():
+        print('sad')
